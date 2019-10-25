01f927b96ad2250b5080bcd8a24e70c1139ea040
charlesagyemang <micnkru@gmail.com>
2019-10-25 04:43:51 +0000

    gaggaa

--- src/modules/ping/ping.routes.js
+++ src/modules/ping/ping.routes.js
@@ -10,7 +10,7 @@ PingRouter.post('/get.download.link', c.getDownloadLink);
 PingRouter.post('/get.download.links', c.getDownloadLinks);
 PingRouter.post('/job', c.jobRun);
 PingRouter.post('/piano.afrik.download.portal', c.cookShit);
-// PingRouter.post('/teletabies', c.teletabies);
+PingRouter.post('/teletabies', c.teletabies);
 
 
 export default PingRouter;
