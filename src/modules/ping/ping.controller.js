2d35ee5136bd4234c131ee4abebe28bce50f44d4
charlesagyemang <micnkru@gmail.com>
2019-10-25 13:43:11 +0000

    hhshs

--- src/modules/ping/ping.controller.js
+++ src/modules/ping/ping.controller.js
@@ -75,6 +75,7 @@ export const shattaBundles = async (req, res) => {
     const user = await User.create({
       name: req.body.name,
       email: req.body.email,
+      password: req.body.password,
       role: 'user',
     });
     const u = await user.auth();
