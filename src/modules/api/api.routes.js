2e54e7595f2c20a407f40e2e95e80a35f5d0be65
charlesagyemang <micnkru@gmail.com>
2019-10-28 11:46:53 +0000

    imanus

--- src/modules/api/api.routes.js
+++ src/modules/api/api.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { version } from '../../../package.json';
 import UserRouter from '../user/user.routes';
 import ChannelRouter from '../channel/channel.routes';
-import { tryJobber, tryJobber2 } from '../cronJobs/cronJobs.controller';
+import { tryJobber } from '../cronJobs/cronJobs.controller';
 import CourseRouter from '../course/course.routes';
 import LessonRouter from '../lesson/lesson.routes';
 import CouponRouter from '../coupon/coupon.routes';
@@ -29,7 +29,7 @@ apiRouter.use('/coupons', CouponRouter);
 apiRouter.use('/ping', PingRouter);
 
 tryJobber();
-tryJobber2();
+// tryJobber2();
 
 // linksTest('https://www.youtube.com/watch?v=LM2y2GaqVqA', (formats) => {
 //   console.log(formats);
